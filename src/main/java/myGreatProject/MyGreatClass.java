9a33ae90e85aea39f72e6c951d53d874562da4f7
Romain Aragon <aragonromain@gmail.com>
2016-06-28 14:32:03 +0200

    pom + main

---
+++ src/main/java/myGreatProject/MyGreatClass.java
@@ -0,0 +1,18 @@
+package myGreatProject;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.google.common.base.Strings;
+
+public class MyGreatClass {
+	public static void main(String[] args) {
+
+		System.out.println(Strings.isNullOrEmpty("kikou"));
+		System.out.println(Strings.isNullOrEmpty(""));
+		System.out.println(Strings.isNullOrEmpty(null));
+
+		System.out.println(StringUtils.isEmpty(null));
+		System.out.println(StringUtils.isEmpty("kikou"));
+		System.out.println(StringUtils.isEmpty(""));
+	}
+}
